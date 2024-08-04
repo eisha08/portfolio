0590edb98796d4336bc1316beba1a3e5b12c6e94
Eisha <eishajha0807@gmail.com>
2024-08-04 22:41:45 +0530

    Prohects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -15,12 +15,14 @@ function Projects({theme}) {
     {
       image: blog,
       title: "AgonyMother",
-      description: "Agony Mother is a full-stack website designed to be a supportive platform where anyone can share their day-to-day thoughts and feelings. As a good listener, Agony Mother provides a safe space for users to express themselves freely. Whether you're venting about a tough day or sharing a joyful moment, Agony Mother is here to listen and offer a sense of community and understanding."
+      description: "Agony Mother is a full-stack website designed to be a supportive platform where anyone can share their day-to-day thoughts and feelings. As a good listener, Agony Mother provides a safe space for users to express themselves freely. Whether you're venting about a tough day or sharing a joyful moment, Agony Mother is here to listen and offer a sense of community and understanding",
+      url:"https://github.com/eisha08/blogSite"
     },
     {
       image: todo,
       title: "DayBreak",
-      description: "Daybreak is a full-stack website designed for users to manage their daily tasks efficiently. With full authentication, users can add, prioritize, and keep track of their tasks in a secure and organized manner. Daybreak ensures that your to-do list is tailored to your needs, helping you stay productive and on top of your responsibilities."
+      description: "Daybreak is a full-stack website designed for users to manage their daily tasks efficiently. With full authentication, users can add, prioritize, and keep track of their tasks in a secure and organized manner. Daybreak ensures that your to-do list is tailored to your needs, helping you stay productive and on top of your responsibilities.",
+      url:"https://github.com/eisha08/DayBreak"
     },
     {
       image: edu,
@@ -30,30 +32,33 @@ function Projects({theme}) {
     {
         image: musica,
         title: "Musica", 
-        description:"Musica is a frontend website dedicated to showcasing the music of underground rappers. Explore and enjoy a diverse range of songs from emerging artists, all in one place with Musica."
+        description:"Musica is a frontend website dedicated to showcasing the music of underground rappers. Explore and enjoy a diverse range of songs from emerging artists, all in one place with Musica.",
+        url:"https://github.com/eisha08/Music-Player"
+        
     },
     {
         image: profile,
         title: "My Portfolio", 
-        description: "My Portfolio Website is a frontend project that showcases my skills, projects, and experiences as a developer. Visitors can learn about my background, view my resume, and contact me for collaboration or job opportunities. This portfolio provides a comprehensive and engaging overview of my professional journey and capabilities."
+        description: "My Portfolio Website is a frontend project that showcases my skills, projects, and experiences as a developer. Visitors can learn about my background, view my resume, and contact me for collaboration or job opportunities. This portfolio provides a comprehensive and engaging overview of my professional journey and capabilities.",
+        url:"https://github.com/eisha08/portfolio"
+
     },
 
 
   ];
 
   return (
-    <div  className="">
-        <div id='project' className={` text-left pl-10 font-bold text-4xl ${theme === 'dark' ? 'text-pink-300' : 'text-purple-700'}  py-10`}>
-            My Work
-        </div>
-    
-    <div  className="flex flex-wrap justify-center items-center min-h-screen">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 md:gap-4 lg:gap-6 ">
-        {projects.map((project, index) => (
-          <Card key={index} image={project.image} title={project.title} description={project.description} />
-        ))}
+    <div className="">
+      <div id='project' className={`text-left pl-10 font-bold text-4xl ${theme === 'dark' ? 'text-pink-300' : 'text-purple-700'} py-10`}>
+        My Work
       </div>
-    </div>
+      <div className="flex flex-wrap justify-center items-center min-h-screen">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 md:gap-4 lg:gap-6">
+          {projects.map((project, index) => (
+            <Card key={index} image={project.image} title={project.title} description={project.description} url={project.url} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
